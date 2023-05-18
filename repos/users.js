62f2cde8034f30e1af8e40dae2e099006938fded
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-18 14:07:18 +0530

    Create and Save users

---
+++ repos/users.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a repository requires filename");
+    }
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (error) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+
+  async create(attr) {
+    const users = await this.getAll();
+    users.push(attr);
+    await fs.promises.writeFile(this.filename, JSON.stringify(users));
+  }
+
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf-8",
+      })
+    );
+  }
+}
+
+const test = async () => {
+  // creates a users repository
+  const repo = new UsersRepository("users.json");
+  // creates a user record in the repo
+  await repo.create({ email: "test@test.com", password: "password" });
+  // gets all the contents of repo
+  const users = await repo.getAll();
+  // logs them
+  console.log(users);
+};
+
+test();
