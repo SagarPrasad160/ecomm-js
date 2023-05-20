eb67ac4a98d71f013074350b538da438d59ecfa9
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-20 15:54:41 +0530

    use multer middleware to process the multipart/form-data and save the image buffer from req.file

--- routes/admin/products/products.js
+++ routes/admin/products/products.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const router = express.Router();
-
 const { validationResult } = require("express-validator");
+const multer = require("multer");
 
 const productsRepo = require("../../../repos/products");
 const { requireTitle, requirePrice } = require("../auth/validators");
-
 const newProductForm = require("../../../views/admin/new");
+
+const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -14,10 +15,24 @@ router.get("/admin/products/new", (req, res) => {
   res.send(newProductForm({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
-  res.send("Created");
-});
+router.post(
+  "/admin/products/new",
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.send(newProductForm({ errors }));
+    }
+
+    const { title, price } = req.body;
+    const image = req.file.buffer.toString("base64");
+
+    await productsRepo.create({ title, price, image });
+
+    res.send("Created");
+  }
+);
 
 module.exports = router;
