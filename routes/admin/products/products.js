b19467bc67279137f742b447a2386bccca75e93a
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-20 18:05:41 +0530

    add requireAuth middleware

--- routes/admin/products/products.js
+++ routes/admin/products/products.js
@@ -4,23 +4,24 @@ const multer = require("multer");
 const productsRepo = require("../../../repos/products");
 const { requireTitle, requirePrice } = require("../auth/validators");
 const newProductForm = require("../../../views/admin/new");
-const { handleErrors } = require("../middlewares");
+const { handleErrors, requireAuth } = require("../middlewares");
 const productListTemplate = require("../../../views/admin/index");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", async (req, res) => {
+router.get("/admin/products", requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productListTemplate({ products }));
 });
 
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(newProductForm({}));
 });
 
 router.post(
   "/admin/products/new",
+  requireAuth,
   upload.single("image"),
   [requireTitle, requirePrice],
   handleErrors(newProductForm),
