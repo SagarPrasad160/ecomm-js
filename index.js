a66dc2929837e9f9099af44cdce077a5b282498a
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-21 19:52:09 +0530

    undo changes in root index.js

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const authRouter = require("./routes/admin/auth/auth");
 const adminProductsRouter = require("./routes/admin/products/products");
 const productsRouter = require("./routes/products");
 const cartRouter = require("./routes/cart");
-
 app.use(express.static("public"));
+// using bodyParser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    name: "session",
     keys: ["afdasfasf"],
   })
 );
 
-app.use("/admin/auth", authRouter);
-app.use("/admin/products", adminProductsRouter);
-app.use("/products", productsRouter);
-app.use("/cart", cartRouter);
+app.use(authRouter);
+app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartRouter);
 
-module.exports = app;
+app.listen(3000, () => {
+  console.log("Listening at port 3000!");
+});
